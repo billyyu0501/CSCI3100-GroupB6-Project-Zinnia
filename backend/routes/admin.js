7baa09bef16c2f341080d0642debc3aa91b569ab
billyyu0501 <66482960+billyyu0501@users.noreply.github.com>
2022-04-13 19:19:30 +0800

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -208,7 +208,7 @@ router.post("/admin/delete/groupChat",async(req,res)=>{
     })
 })
 
-router.post("/admin/:userId/profile", (req, res) => {
+router.post("/admin/:userId/profile", async(req, res) => {
     if (req.body.changeUsername == "" && req.body.changeDescription == "" && req.body.photo == "" && req.body.changePassword == ""){
         return res.status(400).send({msg: "Please fill in the blanks if you want to update profile."})
     }
@@ -216,45 +216,46 @@ router.post("/admin/:userId/profile", (req, res) => {
         User.findOne({ username: req.body.changeUsername }, (err, user) => {
             if (user){
                 return res.status(401).send({msg: "Username existed. Please choose another username."})
+            }else{
+                User.findOne({ userId: req.params.userId, }, async (err, user) => {
+                    if(!user){
+                        return res.status(401).send({msg: "We cannot find this user to update."})
+                    }
+                    else {
+                        //username update
+                        if (req.body.changeUsername == ""){
+                            user.username = req.body.username
+                        } else {user.username = req.body.changeUsername}
+                        //description update
+                        if (req.body.changeDescription == ""){
+                            user.description = req.body.description
+                        } else {user.description = req.body.changeDescription}
+        
+                         //password update
+                        if (req.body.changePassword == ""){
+                            user.password = req.body.password
+                        } 
+                        else {
+                            const hashedPassword = await bcrypt.hash(req.body.changePassword, 10)
+                            user.password = hashedPassword
+                        }
+        
+                        //photo update
+                        user.photo = req.body.photo
+                        user.save(() => {
+                            if (err){
+                                return res.status(500).send({msg: err.message});
+                            }
+                            else{
+                                return res.status(201).send({msg: "Profile has been update"});
+            
+                            }
+                        })
+                    }
+                })
             }  
         })
 
-        User.findOne({ userId: req.params.userId, }, async (err, user) => {
-            if(!user){
-                return res.status(401).send({msg: "We cannot find this user to update."})
-            }
-            else {
-                //username update
-                if (req.body.changeUsername == ""){
-                    user.username = req.body.username
-                } else {user.username = req.body.changeUsername}
-                //description update
-                if (req.body.changeDescription == ""){
-                    user.description = req.body.description
-                } else {user.description = req.body.changeDescription}
-
-                 //password update
-                if (req.body.changePassword == ""){
-                    user.password = req.body.password
-                } 
-                else {
-                    const hashedPassword = await bcrypt.hash(req.body.changePassword, 10)
-                    user.password = hashedPassword
-                }
-
-                //photo update
-                user.photo = req.body.photo
-                user.save(() => {
-                    if (err){
-                        return res.status(500).send({msg: err.message});
-                    }
-                    else{
-                        return res.status(201).send({msg: "Profile has been update"});
-    
-                    }
-                })
-            }
-        })
 
 
     }
